a81d35fbd4af3c05a210ec8aa88375a100a76ef4
sen08 <pseeun08@gmail.com>
2020-09-21 19:50:51 +0900

    제발

--- TDW/src/main/java/com/example/sen/DemoApplication2.java
+++ TDW/src/main/java/com/example/sen/DemoApplication2.java
@@ -8,7 +8,7 @@ public class DemoApplication2 {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication2.class, args);
-		System.out.println("제발..");
+		System.out.println("제발.. ㅡㅡ");
 	}
 
 }
